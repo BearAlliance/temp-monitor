fca5703c95ce6f13e42c372d6b64396f77a36fbd
Nick Cacace <nickcacace@gmail.com>
2016-02-03 21:01:52 -0500

    Add temperature readings

--- tweetTemp.py
+++ tweetTemp.py
@@ -1,13 +1,8 @@
 import twitter
 import datetime
+import time
+from RPi import GPIO
 
-# Returns temperature reading as a string
-def getTempReading():
-	# don't have the sensor yet, just return a value
-
-	temp = 68.5
-
-	return str(temp)
 
 # Saves the temperature to a database for fun graphs later
 def saveToDb(temp):
@@ -20,6 +15,7 @@ def getDateTimeString():
 	now = datetime.datetime.now()
 	return now.strftime('%m/%d %-I:%M%p')
 
+# Tweets the given temperature
 def tweetTemperature(temp):
 	dateTime = getDateTimeString()
 
@@ -45,11 +41,119 @@ def tweetTemperature(temp):
 		print 'APIconnection failure'
 
 
+# Temperature reading
+#################################################
+
+def bin2dec(string_num):
+    return str(int(string_num, 2))
+
+def cTof(cTemp):
+	return str((float(cTemp) * 1.8) + 32)
+
+
+def readTemp():
+	data = []
+
+	GPIO.setmode(GPIO.BCM)
+
+	GPIO.setup(4,GPIO.OUT)
+	GPIO.output(4,GPIO.HIGH)
+	time.sleep(0.025)
+	GPIO.output(4,GPIO.LOW)
+	time.sleep(0.02)
+
+	GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+	for i in range(0,500):
+	    data.append(GPIO.input(4))
+
+	bit_count = 0
+	tmp = 0
+	count = 0
+	HumidityBit = ""
+	TemperatureBit = ""
+	crc = ""
+
+	try:
+		while data[count] == 1:
+			tmp = 1
+			count = count + 1
+
+		for i in range(0, 32):
+			bit_count = 0
+
+			while data[count] == 0:
+				tmp = 1
+				count = count + 1
+
+			while data[count] == 1:
+				bit_count = bit_count + 1
+				count = count + 1
+
+			if bit_count > 3:
+				if i>=0 and i<8:
+					HumidityBit = HumidityBit + "1"
+				if i>=16 and i<24:
+					TemperatureBit = TemperatureBit + "1"
+			else:
+				if i>=0 and i<8:
+					HumidityBit = HumidityBit + "0"
+				if i>=16 and i<24:
+					TemperatureBit = TemperatureBit + "0"
+
+	except:
+		print "ERR_RANGE"
+		return "Error"
+
+	try:
+		for i in range(0, 8):
+			bit_count = 0
+
+			while data[count] == 0:
+				tmp = 1
+				count = count + 1
+
+			while data[count] == 1:
+				bit_count = bit_count + 1
+				count = count + 1
+
+			if bit_count > 3:
+				crc = crc + "1"
+			else:
+				crc = crc + "0"
+	except:
+		print "ERR_RANGE"
+		return "Error"
+
+	Humidity = bin2dec(HumidityBit)
+	Temperature = bin2dec(TemperatureBit)
+
+	if int(Humidity) + int(Temperature) - int(bin2dec(crc)) == 0:
+		# print "Humidity:"+ Humidity +"%"
+		# print "Temperature:"+ Temperature +"C"
+		fTemp = cTof(Temperature)
+		# print "Temperature:"+ fTemp +"F"
+		return fTemp
+	else:
+		print "ERR_CRC"
+		return "Error"
+
+# Returns temperature reading as a string
+# Hit sensor again if error is returned
+def getTempReading():
+	result = readTemp()
+	while(result == "Error"):
+		result = readTemp()
+	
+	return result
+
+
 #################################################
 
 
 # Get the temperature
 currentTemp = getTempReading()
+# print "Temperature: " + currentTemp + "F"
 
 # Save it to a dabase
 saveToDb(currentTemp)
